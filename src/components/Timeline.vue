720734866f327a2375e57a8aca35e39f5c8712df
Renato66 <revi66@hotmail.com>
2019-08-12 00:27:49 -0300

    updated photos

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -46,8 +46,8 @@
       <iframe class="white" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D10203204040314104%26set%3Da.4213882857609%26type%3D3&width=500&show_text=true&appId=998943660228637&height=424" width="500" height="424" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
     </v-dialog>
     <v-dialog v-model="open['colombia']" content-class="text-xs-center">
-      <v-carousel v-if="open['colombia']">
-        <v-carousel-item v-for="photo in colombiaPhotos" :key="photo" :src="`/img/colombia/${photo}.jpeg`"></v-carousel-item>
+      <v-carousel v-if="open['colombia']" hide-delimiters :cycle="false">
+        <v-carousel-item v-for="photo in colombia" :key="photo" :src="`/img/colombia/photo${photo}.jpeg`"></v-carousel-item>
       </v-carousel>
     </v-dialog>
   </v-container>
@@ -62,18 +62,7 @@ export default {
   },
   data () {
     return {
-      colombiaPhotos: [
-        'sign1',
-        'sign2',
-        'food1',
-        'food2',
-        'food3',
-        'food4',
-        'friends1',
-        'friends2',
-        'friends3',
-        'photo1'
-      ],
+      colombia: 8,
       open: {
         fatec: false,
         colombia: false
