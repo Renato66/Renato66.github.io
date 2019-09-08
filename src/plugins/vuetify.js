976c39dfb92274e234c59472d6d8987a7cd5ca49
Renato66 <revi66@hotmail.com>
2019-09-08 17:23:59 -0300

    colors changed

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,8 +4,8 @@ import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
+    primary: '#22c6a0',
+    secondary: '#0e0d1c',
     accent: '#82B1FF',
     error: '#FF5252',
     info: '#2196F3',
