b19fbdcee7df59f3045fb8eefdff4ffb5692aa77
Renato66 <revi66@hotmail.com>
2020-03-01 18:36:43 +0000

    Deploying to master from source - 5078504beed344a59910d1713c3618a92f149b63 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6702f6568f68538e0cc2e919cb6df7a1.js"
+  "/precache-manifest.5e0c527fd21462d38de0c6dab9a6e36b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portifolio"});
