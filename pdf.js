daf0243a6f0a48b984377a3bb716b87eebc9ebfd
Renato Vicente <revi66@hotmail.com>
2020-03-11 12:06:16 -0300

    lint

--- pdf.js
+++ pdf.js
@@ -1,19 +1,19 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
-var fs = require('fs');
-var dir = './dist/download';
+const path = require('path')
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+const dir = './dist/download';
 
 (async () => {
-  if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
   }
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.goto(`file:${path.join(__dirname, 'dist/resume/index.html')}`);
-  await page.pdf({path: `${dir}/renato-vicente-cv-pt.pdf`, format: 'A4'});
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.goto(`file:${path.join(__dirname, 'dist/en/resume/index.html')}`);
-  await page.pdf({path: `${dir}/renato-vicente-cv-en.pdf`, format: 'A4'});
-  await browser.close();
-})();
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' })
+  await page.goto(`file:${path.join(__dirname, 'dist/resume/index.html')}`)
+  await page.pdf({ path: `${dir}/renato-vicente-cv-pt.pdf`, format: 'A4' })
+  await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' })
+  await page.goto(`file:${path.join(__dirname, 'dist/en/resume/index.html')}`)
+  await page.pdf({ path: `${dir}/renato-vicente-cv-en.pdf`, format: 'A4' })
+  await browser.close()
+})()
