d8a321b069f9f517b6bd0635a39e888f3c378969
Renato Vicente <revi66@hotmail.com>
2020-03-16 19:07:33 -0300

    google analytics added

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,10 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-160642701-1'
+    }]
   ],
   /*
   ** Nuxt.js modules
