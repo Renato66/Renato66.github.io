5d2db604a4f90c612113ad01054c5da6410ca339
Renato Vicente <revi66@hotmail.com>
2020-07-10 02:18:09 -0300

    remove unused code

--- pdf.js
+++ pdf.js
@@ -9,11 +9,9 @@ const dir = './dist/download';
   }
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' })
-  await page.goto(`file:${path.join(__dirname, 'dist/resume/index.html')}`)
+  await page.goto(`file:${path.join(__dirname, 'dist/resume/index.html')}`, { waitUntil: 'networkidle2' })
   await page.pdf({ path: `${dir}/renato-vicente-cv-pt.pdf`, format: 'A4' })
-  await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' })
-  await page.goto(`file:${path.join(__dirname, 'dist/en/resume/index.html')}`)
+  await page.goto(`file:${path.join(__dirname, 'dist/en/resume/index.html')}`, { waitUntil: 'networkidle2' })
   await page.pdf({ path: `${dir}/renato-vicente-cv-en.pdf`, format: 'A4' })
   await browser.close()
 })()
