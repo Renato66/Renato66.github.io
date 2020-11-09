cca3df64312f92a9141a29724636f13bff1d86cf
Renato66 <Renato66@users.noreply.github.com>
2020-11-09 03:41:36 +0000

    Deploying to master from source @ 5e631cb6ee92021500d82eccadfb09e89355479a 🚀

---
+++ _nuxt/pages/index.2bb4c40.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{502:function(n,l,o){"use strict";o.r(l);var e=o(491).default,t=o(50),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
