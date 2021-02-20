d13a2345546cd38fc7186a67f4e480f348a45221
Renato66 <revi66@hotmail.com>
2021-02-20 04:21:15 -0300

    chore: add preload

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'preload', as: 'style', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap' }
     ]
   },
