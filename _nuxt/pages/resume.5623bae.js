96d5bd80a039519492ee00f477294f47a08313bd
Renato66 <Renato66@users.noreply.github.com>
2023-04-02 21:49:10 +0000

    Deploying to master from source @ e0d45eff8cbbdef7533a64c54b7010cb9bcb4004 🚀

---
+++ _nuxt/pages/resume.5623bae.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{504:function(n,l,o){"use strict";o.r(l);var e=o(493).default,t=o(50),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
